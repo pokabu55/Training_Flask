d061a17ab58be422143a49818d8e8cc70463f88d
pokabu55 <okazumaru@gmail.com>
2022-02-05 10:32:51 +0900

    Flask2 からのメソッドの書き方

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -12,3 +12,9 @@ def index():
     endpoint="hello-endpoint") # Endpoint
 def hello():
     return "hello, world!!"
+
+# Flask2 から @app.route(methods=["Get"]) を下記のように書けるようになった
+@app.get("/flask2")
+@app.post("/flask2")
+def hello():
+    return "flask2 specification"
