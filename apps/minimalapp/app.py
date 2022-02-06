8adb8769aed661245189728577305bb5ba525ca3
pokabu55 <okazumaru@gmail.com>
2022-02-06 15:50:21 +0900

    url_for 関数を使ってURLを生成する

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -19,6 +19,15 @@ def hello(name):
 def show_name(name):
     # 変数をテンプレートに渡す
     return render_template("index.html", name=name)
+
+# url_for 関数でurlを出力してみる
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=ichiro
+    print(url_for("show_name", name="ichiro", page="1"))
 
 # Flask2 から @app.route(methods=["Get","Post"]) を下記のように書けるようになった
 @app.get("/flask2")
