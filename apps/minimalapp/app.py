2c47cf945225683b334319ffe3524a4de4819bd7
pokabu55 <okazumaru@gmail.com>
2022-02-06 17:12:04 +0900

    flash メッセージの実装

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,18 @@
 from cgitb import reset
 from crypt import methods
-from flask import Flask, render_template, url_for, current_app, g, request, redirect
+from unicodedata import is_normalized
+# import 文が長くなるため改行
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    current_app,
+    g,
+    request,
+    redirect,
+    flash
+)
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 
@@ -62,9 +74,41 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
+        # form 属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+        # 入力をチェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザーネームは必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
         # メールを送る（後に実装）
 
-        # contactエンドポイントへリダイレクトする
+        # 問い合わせ完了エンドポイントへリダイレクトする
+        flash("お問い合わせありがとうございました。")
         return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
+
+# SECRET_KEY を追加する
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
